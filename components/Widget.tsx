a41ae33ffda13757b0f0a31ed00970c81baff7af
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2022-09-24 21:09:23 +0200

    [widget] Lessen the bottom margin on search

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -4,13 +4,13 @@ import { TwitterTimelineEmbed } from "react-twitter-embed";
 export default function Widget() {
   return (
     <section>
-      <div className="py-3 px-3 rounded-full flex mb-4 bg-gray-100">
-        <SearchIcon className="w-5 mx-3 text-gray-400" />
+      <div className="py-3 px-3 rounded-full flex mb-2 bg-gray-100">
+        <SearchIcon className="w-5 mx-2 text-gray-500" />
 
         <input
           type="text"
           placeholder="Search Twitter"
-          className="outline-none placeholder:text-gray-400 flex-1"
+          className="outline-none placeholder:text-gray-500 flex-1"
         />
       </div>
       <TwitterTimelineEmbed
