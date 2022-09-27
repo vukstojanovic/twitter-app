426b24f5b3e515f1b2ac97e34b07758fffb79dee
vukstojanovic <82164509+vukstojanovic@users.noreply.github.com>
2022-09-28 00:55:10 +0200

    [tweet-component] Add tweet component

--- components/Feed/Feed.tsx
+++ components/Feed/Feed.tsx
@@ -1,7 +1,18 @@
 import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import TweetBox from "../TweetBox/TweetBox";
+import Tweet from "../Tweet/Tweet";
 
 export default function Feed() {
+  const tweetData = {
+    text: "This is a DEMO TWEET",
+    username: "Sonny Sangha",
+    profileImg:
+      "https://pbs.twimg.com/profile_images/1339192504382590976/2WxMn8cm_400x400.jpg",
+    image:
+      "https://media-exp1.licdn.com/dms/image/C4D16AQFBTEZH9739Ng/profile-displaybackgroundimage-shrink_200_800/0/1610818787022?e=2147483647&v=beta&t=H77pitXNXNaBtNvj35KCi7seCtMoYbLq85MWJweDmh4",
+    _createdAt: new Date(),
+  };
+
   return (
     <section className="col-span-7 p-3 overflow-auto md:col-span-5">
       <div className="flex justify-between items-center">
@@ -9,6 +20,7 @@ export default function Feed() {
         <ArrowPathIcon className="w-7 cursor-pointer hover:rotate-180 active:scale-125 hover:text-twitter ease-in-out duration-300" />
       </div>
       <TweetBox />
+      <Tweet {...tweetData} />
     </section>
   );
 }
